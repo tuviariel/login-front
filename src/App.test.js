7403cd9481cd95ad10813cd2c1a88510675a37e3
Tuvia Ariel <tuviaa109@gmail.com>
2024-02-25 00:53:51 +0200

    with front test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders welcome', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = screen.getByText(/welcome/i);
+  expect(titleElement).toBeInTheDocument();
 });
